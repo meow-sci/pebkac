b5ca89e5d5585af6c988397d09ef372654123526
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2025-11-22 23:46:24 -0500

    license up

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meow.science.fail',
-  base: '/pebkac',
-  integrations: [preact({ compat: true })]
+  base: '/pebkac/',
+  trailingSlash: 'always',
+  integrations: [
+    preact({ compat: true }),
+  ],
 });
