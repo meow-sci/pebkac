9680fe03ca45bdb5473f25dd69619150ce0e4445
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2025-12-18 08:13:55 -0500

    fixed so that Id attributes which use a path to textures get rewritten with a "../Core/" prefix

--- src/ts/xml/fixPathsToCore.ts
+++ src/ts/xml/fixPathsToCore.ts
@@ -1,8 +1,10 @@
 import { isElementNode } from "./isXmlNodeTypeGuards";
 
 /**
- * Recurisively walk the Document, looking for any "Path" attribute and mutate the existing
- * value with a prefix of `../Core/`
+ * Recurisively walk the Document, looking for...
+ * 
+ * 1. any "Path" attribute and mutate the existing value with a prefix of `../Core/`
+ * 2. any "Id" attribute that starts with "Texture" (which is a path), mutate with a prefix of `../Core/`
  * 
  * @param doc Document to mutate
  * @returns 
@@ -19,11 +21,20 @@ export function fixPathsToCore(doc: Document): void {
 }
 
 function fixElementPaths(element: Element): void {
+
   // Check if this element has a "Path" attribute
   if (element.hasAttribute("Path")) {
     const currentPath = element.getAttribute("Path");
     if (currentPath && !currentPath.startsWith("../Core/")) {
       element.setAttribute("Path", `../Core/${currentPath}`);
+    }
+  }
+  
+  // Check if this element has a "Id" attribute that starts with "Texture"
+  if (element.hasAttribute("Id")) {
+    const currentId = element.getAttribute("Id");
+    if (currentId && currentId.startsWith("Texture") && !currentId.startsWith("../Core/")) {
+      element.setAttribute("Id", `../Core/${currentId}`);
     }
   }
 
